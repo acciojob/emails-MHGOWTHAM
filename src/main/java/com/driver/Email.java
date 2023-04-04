29d0a87f4dbc74c07fe6c83b098e47f9cab5c7d1
GOWTHAM M H <gowthammh02@gmail.com>
2023-04-04 19:29:22 +0530

    Email submission

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,31 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(!newPassword.equals(oldPassword)){
+            return;
+        }
+        if(conditions(newPassword)){
+            this.password=newPassword;
+            return;
+        }
+    }
+
+    private boolean conditions(String newPassword) {
+        if(newPassword.length()<8) return false;
+        boolean upperCase=false;
+        boolean lowerCase = false;
+        boolean digit = false;
+        boolean specialCharacter = false;
+        for(int i=0;i<newPassword.length();i++){
+            if(newPassword.charAt(i)>='A'&&newPassword.charAt(i)<='Z')
+                upperCase=true;
+            else if(newPassword.charAt(i)>='a'&&newPassword.charAt(i)<='z')
+               lowerCase=true;
+            else if (newPassword.charAt(i)>='0'&&newPassword.charAt(i)<='9')
+                digit=true;
+
+            else specialCharacter=true;
+        }
+        return upperCase&&lowerCase&&digit&&specialCharacter;
     }
 }
